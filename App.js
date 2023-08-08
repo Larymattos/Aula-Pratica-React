2b1e78b9949743eed2d4a9dca9ea84d3ef2442a1
LaryMattos <120655144+Larymattos@users.noreply.github.com>
2023-08-08 19:08:51 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Countdown from './Countdown';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Minha Aplicação</h1>
+      <Countdown initialTime={10} /> 
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+/*import logo from './logo.svg';
+import './App.css';
+import './exercicio'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
